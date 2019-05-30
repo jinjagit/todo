390677e063ef89d7bdd890ebc2b4cb7d49c79430
Simon Tharby <jinjamail@gmail.com>
2019-05-30 19:51:58 +0100

    try smaller font for todosIndex (to improve mobile view)

--- src/todosIndex.js
+++ src/todosIndex.js
@@ -4,7 +4,7 @@ const todosIndex = (todos) => {
   for(let i = 0; i < todos.length; i++) {
     let div = document.createElement('div');
     div.classList.add('minTodoDiv');
-    let title = document.createElement('h1');
+    let title = document.createElement('h2');
     title.innerHTML = todos[i].title;
     content.appendChild(div);
     div.appendChild(title);

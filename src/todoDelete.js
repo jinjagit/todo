07c4248a2eeae95f0450993fe976475659a3948e
Simon Tharby <jinjamail@gmail.com>
2019-05-31 21:38:54 +0100

    use css to change background image by changing element id value

--- src/todoDelete.js
+++ src/todoDelete.js
@@ -1,8 +1,7 @@
 const todoDelete = (todos, thisId) => {
   let id = thisId.slice(7);
   let checkBox = document.getElementById(thisId);
-  checkBox.style.background = "url('../img/tick.jpg')";
-  checkBox.style.backgroundSize = 'contain';
+  checkBox.id = 'deleted';
   setTimeout(function() {
     todos.splice(todos.indexOf(todos.find(e => e.id == id)), 1);
     let todoDiv = document.getElementById(id);
